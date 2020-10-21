1de8a042c9c389bb9026dfcb570486e5193d8f78
Paul Butcher <paul.butcher@gmail.com>
2020-10-21 09:54:12 +0100

    Add size option

--- generate_tracery.py
+++ generate_tracery.py
@@ -7,7 +7,8 @@ with open('data/lights.txt', 'r') as colour_file:
    lights = [x.strip() for x in colour_file.readlines()]
 
 print(json.dumps({
-    "origin":["[this_dark:#dark#]  [this_light:#light#] #this_dark# and #this_light# {img https://mitzi-del-bra.glitch.me/houndstooth.png?c1=#this_dark#&c2=#this_light#}"],
+    "size": ["1","2","3","4","5","6","7","8","9"],
+    "origin":["[this_dark:#dark#]  [this_light:#light#] [this_size:#size#] #this_dark# and #this_light# 2x#this_size# threads per colour {img https://mitzi-del-bra.glitch.me/houndstooth.png?c1=#this_dark#&c2=#this_light#&size=#this_size#}"],
     "dark": darks,
     "light": lights
     }, indent=2))
